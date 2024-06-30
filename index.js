6eea8c4cc90540bd172902dbcebee00014c32204
Sebastian Galeano <sebascarp9@gmail.com>
2024-06-30 10:45:05 -0300

    Middleware errorHandler use

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { PRODUCTS } from './data.js';
 import dotenv from 'dotenv';
 import { productRoutes } from './routes/producRouter.js';
+import { errorHandler } from './middlewares/errorHandler.js';
 
 dotenv.config();
 const SERVER_PORT = process.env.SERVER_PORT || 2010;
@@ -9,8 +10,9 @@ const SERVER_PORT = process.env.SERVER_PORT || 2010;
 const app = express();
 app.use(express.json());
 
-app.use('/api',productRoutes(PRODUCTS))
+app.use('/api', productRoutes(PRODUCTS));
+app.use(errorHandler);
 
 app.listen(SERVER_PORT, () => {
     console.log(`Server on listening on port ${SERVER_PORT}`);
-})
+});
--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,10 +1,18 @@
 export const ERROR_HANDLER = {
-    defaultError: (response, error) =>{
+    defaultError: (response, error) => {
         response
-           .status(500)
-           .json({
-            success: false,
-            message: error.message
-           })
+            .status(500)
+            .json({
+                success: false,
+                message: error.message
+            })
     }
+}
+
+export const errorHandler = (error, _request, response, _next) => {
+    console.error('Error Handler');
+
+    const handler = ERROR_HANDLER[error.name] || ERROR_HANDLER.defaultError;
+
+    handler(response, error);
 }
