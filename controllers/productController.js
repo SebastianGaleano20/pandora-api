715ef03820d1be50ee290ed06d6019776db8115c
Sebastian Galeano <sebascarp9@gmail.com>
2024-06-30 09:48:32 -0300

    Controllers finished

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,19 +4,51 @@ export const productController = (PRODUCTS) => {
     const getProducts = ((_request, response) => {
         return response.json(PRODUCTS);
     });
-
-    const createProduct = ((req, res) => {
-        const newProduct = req.body;
+    const createProduct = ((request, response) => {
+        const newProduct = request.body;
         const products = structuredClone(PRODUCTS);
         products.push({
             id: uuidv4(),
             ...newProduct
         })
-        return res.status(201).json(products);
+        return response.status(201).json(products);
     })
-    
+    const getProductById = ((request, response) => {
+        const { id } = request.params;
+        const product = PRODUCTS.find((product) => product.id === id);
+        if (!product) {
+            return response.status(404).json({ message: 'Product not found' });
+        }
+        return response.status(200).json(product);
+    })
+    const updateProduct = ((request, response) => {
+        const { id } = request.params;
+        const products = structuredClone(PRODUCTS);
+        const product = products.find((product) => product.id === id)
+        if (!product) {
+            return response.status(404).json({ message: 'Product not found' })
+        }
+        const updateProduct = request.body;
+        const index = products.indexOf(product);
+        products.splice(index, 1, { id: product.id, ...updateProduct })
+        return response.status(200).json(products);
+    })
+    const deleteProduct = ((req, res) => {
+        const { id } = req.params;
+        const products = structuredClone(PRODUCTS);
+        const product = products.find((product) => product.id === id)
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found' })
+        }
+        const index = products.indexOf(product)
+        products.splice(index, 1)
+        return res.status(200).json(products);
+    })
     return {
         getProducts,
-        createProduct
+        createProduct,
+        getProductById,
+        deleteProduct,
+        updateProduct
     }
 }
--- routes/producRouter.js
+++ routes/producRouter.js
@@ -3,42 +3,14 @@ import { productController } from '../controllers/productController.js';
 
 export const productRoutes = (PRODUCTS) => {
     const productRouter = Router();
-    const { getProducts, createProduct } = productController(PRODUCTS);
+    const { getProducts, createProduct, getProductById, deleteProduct, updateProduct } = productController(PRODUCTS);
     productRouter.route('/products')
         .get(getProducts)
         .post(createProduct)
 
     productRouter.route('/products/:id')
-        .get((req, res) => {
-            const { id } = req.params;
-            const product = PRODUCTS.find((product) => product.id === id);
-            if (!product) {
-                return res.status(404).json({ message: 'Product not found' });
-            }
-            return res.status(200).json(product);
-        })
-        .put((req, res) => {
-            const { id } = req.params;
-            const products = structuredClone(PRODUCTS);
-            const product = products.find((product) => product.id === id)
-            if (!product) {
-                return res.status(404).json({ message: 'Product not found' })
-            }
-            const updateProduct = req.body;
-            const index = products.indexOf(product);
-            products.splice(index, 1, { id: product.id, ...updateProduct })
-            return res.status(200).json(products);
-        })
-        .delete((req, res) => {
-            const { id } = req.params;
-            const products = structuredClone(PRODUCTS);
-            const product = products.find((product) => product.id === id)
-            if (!product) {
-                return res.status(404).json({ message: 'Product not found' })
-            }
-            const index = products.indexOf(product)
-            products.splice(index, 1)
-            return res.status(200).json(products);
-        })
+        .get(getProductById)
+        .put(updateProduct)
+        .delete(deleteProduct)
     return productRouter;
 }
