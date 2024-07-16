48de8e8aff87f6c8207431bf629cfd8e49ae55d5
Sebastian Galeano <sebascarp9@gmail.com>
2024-07-16 10:00:10 -0300

    Joi configuration for user and category models

--- schemas/productSchemas.js
+++ schemas/productSchemas.js
@@ -13,3 +13,16 @@ export const productSchemas = Joi.object({
 export const idProductSchema = Joi.object({
     id: Joi.string().patern(/^[0-9]+$/,'id must be a number').required()
 })
+
+export const categorySchema = Joi.object({
+    name: Joi.string().required(),
+    id: Joi.string().patern(/^[0-9]+$/,'id must be a number').required()
+})
+
+export const userSchema = Joi.object({
+    firstName: Joi.string().max(80).required(),
+    secondName: Joi.string().max(80).required(),
+    lastName: Joi.string().max(80).required(),
+    email: Joi.string().email().required(),
+    isAdmin: Joi.boolean()
+})
