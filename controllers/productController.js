0f1cce0ee18b85e27ccb3ed4ba8ec086ec5e9fa4
Sebastian Galeano <sebascarp9@gmail.com>
2024-07-16 09:25:15 -0300

    update controller prisma configuration

--- controllers/productController.js
+++ controllers/productController.js
@@ -58,18 +58,30 @@ export const productController = (PRODUCTS) => {
         }
     }
 
-    const updateProduct = ((request, response) => {
-        const { id } = request.params;
-        const products = structuredClone(PRODUCTS);
-        const product = products.find((product) => product.id === id)
-        if (!product) {
-            return response.status(404).json({ message: 'Product not found' })
+    const updateProduct = async (request, response, next) => {
+        const { id } = request.params
+        const productId = Number(id)
+        const newProductData = request.body
+
+        try {
+            const product = await prisma.products.update({
+                where: {
+                    id: productId
+                },
+                data: newProductData
+            }
+            )
+            const responseFormat = {
+                data: product,
+                message: 'Product update successfully'
+            }
+            return response.status(200).json(responseFormat);
+        } catch (error) {
+            next(error)
+        } finally {
+            await prisma.$disconnect()
         }
-        const updateProduct = request.body;
-        const index = products.indexOf(product);
-        products.splice(index, 1, { id: product.id, ...updateProduct })
-        return response.status(200).json(products);
-    })
+    }
 
     const deleteProduct = async (request, response, next) => {
         const { id } = request.params
