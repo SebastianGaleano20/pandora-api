76fbe780b1c07f57f47dd449d60a4436213b164b
SebastianGaleano20 <sebascarp9@gmail.com>
2024-07-16 22:14:42 -0300

    categories controllers get/patch/delete added

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -36,8 +36,79 @@ export const categoryController = () => {
         }
     }
 
+    const getCategoryById = async (request, response, next) => {
+        const { id } = request.params
+        const categoryId = Number(id)
+        try {
+            const category = await prisma.categories.findUnique({
+                where: {
+                    id: categoryId
+                }
+            })
+            const responseFormat = {
+                data: category,
+                message: 'category retrieved successfully'
+            }
+            return response.status(200).json(responseFormat);
+        } catch (error) {
+            next(error)
+        } finally {
+            await prisma.$disconnect()
+        }
+    }
+
+
+    const updateCategory = async (request, response, next) => {
+        const { id } = request.params
+        const categoryId = Number(id)
+        const newCategoryData = request.body
+
+        try {
+            const category = await prisma.categories.update({
+                where: {
+                    id: categoryId
+                },
+                data: newCategoryData
+            }
+            )
+            const responseFormat = {
+                data: category,
+                message: 'category update successfully'
+            }
+            return response.status(200).json(responseFormat);
+        } catch (error) {
+            next(error)
+        } finally {
+            await prisma.$disconnect()
+        }
+    }
+
+    const deleteCategory = async (request, response, next) => {
+        const { id } = request.params
+        const categorytId = Number(id)
+        try {
+            const category = await prisma.categories.delete({
+                where: {
+                    id: categorytId
+                }
+            })
+            const responseFormat = {
+                data: category,
+                message: 'category delete successfully'
+            }
+            return response.status(200).json(responseFormat);
+        } catch (error) {
+            next(error)
+        } finally {
+            await prisma.$disconnect()
+        }
+    }
+
     return {
         getCategories,
-        createCategory
+        createCategory,
+        updateCategory,
+        deleteCategory,
+        getCategoryById
     }
 }
--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -3,11 +3,15 @@ import { categoryController } from '../controllers/categoryController.js';
 
 export const categoriesRouter = () => {
     const categoryRouter = Router();
-    const { getCategories, createCategory } = categoryController();
+    const { getCategories, createCategory, updateCategory, deleteCategory, getCategoryById } = categoryController();
 
     categoryRouter.route('/categories')
         .get(getCategories)
         .post(createCategory)
-
+    
+    categoryRouter.route('/categories/:id')
+        .get(getCategoryById)
+        .patch(updateCategory)
+        .delete(deleteCategory)
     return categoryRouter
 }
