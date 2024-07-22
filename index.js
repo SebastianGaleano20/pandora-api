e946335dbe9e16d5b2135a49a9ce6ebe1b3ed9f7
SebastianGaleano20 <sebascarp9@gmail.com>
2024-07-22 14:38:16 -0300

    comments index deleted

--- index.js
+++ index.js
@@ -8,13 +8,12 @@ import { userRouter } from './routes/userRouter.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { purchaseRouter } from './routes/purchaseRouter.js';
 
-//Configuracion de mi variable de entorno PORT
 dotenv.config();
 const SERVER_PORT = process.env.SERVER_PORT || 2010;
 
-const app = express(); //Creacion de mi servidor
+const app = express();
 
-app.use(express.json()); //Middleware que permite leer JSON del request.body
+app.use(express.json());
 app.use(cors({
     origin: '*',
     methods: 'GET,POST,PATH,DELETE'
@@ -24,15 +23,14 @@ app.use(exjwt({
     secret: process.env.SECRET_KEY,
     algorithms: ['HS256']
 }).unless({
-    path: ['/api/login', '/api/register']
+    path: ['/api/login', '/api/register', '/api/products', '/api/category']
 })
 )
 
-app.use('/api', productRoutes(), userRouter(), categoryRouter(), purchaseRouter() ) //Uso de mi router
+app.use('/api', productRoutes(), userRouter(), categoryRouter(), purchaseRouter())
 
-app.use(errorHandler);  //Middleware de errores
+app.use(errorHandler);
 
-//Servidor en escucha
 app.listen(SERVER_PORT, () => {
     console.log(`Server on listening on port ${SERVER_PORT}`);
 });
