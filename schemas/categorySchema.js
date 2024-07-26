1eb8cdb5ba9001437b1b4310444196168125d5b8
Sebastian Galeano <126632709+SebastianGaleano20@users.noreply.github.com>
2024-07-26 09:31:27 -0300

    Update categorySchema.js

--- schemas/categorySchema.js
+++ schemas/categorySchema.js
@@ -1,17 +1,21 @@
 import Joi from 'joi'
 
 export const bodyCategorySchema = Joi.object({
-    body: Joi.object({
-        name: Joi.string().required()
+        name: Joi.string().required().messages({
+        'string.base': 'Name must be a string',
+        'string.empty': 'Name cannot be empty',
+        'any.required': 'Name is required'
     })
 })
 export const idCategorySchema = Joi.object({
-    params: ({
-        id: Joi.string().pattern(/^[0-9]+$/, 'id must be a number').required()
+    id: Joi.string().pattern(/^[0-9]+$/).required().messages({
+        'string.pattern.base': 'ID must be a number',
+        'string.empty': 'ID cannot be empty',
+        'any.required': 'ID is required'
     })
-})
+});
 
 export const updateCategorySchema = Joi.object({
-    body: bodyCategorySchema.extract('body'),
-    params: idCategorySchema.extract('params')
-})
+    body: bodyCategorySchema,
+    params: idCategorySchema
+});
